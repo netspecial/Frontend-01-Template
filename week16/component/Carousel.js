0dfbb9cc4e5d22a7b847acb1d9072c6f98617e5a
Maohua Sun <netspecial@hotmail.com>
2020-08-02 18:26:47 +0800

    update "flick"

--- week16/component/Carousel.js
+++ week16/component/Carousel.js
@@ -67,9 +67,9 @@ export class Carousel {
                 let direction = 0;
                 let dx = event.clientX - event.startX;
 
-                if(dx + offset > 250) {
+                if(dx + offset > 250 || dx > 0 && event.isFlick) {
                     direction = 1;
-                } else if(dx + offset < -250) {
+                } else if(dx + offset < -250 || dx < 0 && event.isFlick) {
                     direction = -1;
                 }
                 timeline.reset();
