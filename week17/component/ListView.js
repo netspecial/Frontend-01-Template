f3edcf0d0d5c8a47da41987133073112477bcb8f
Maohua Sun <netspecial@hotmail.com>
2020-08-04 22:53:17 +0800

    commit component

---
+++ week17/component/ListView.js
@@ -0,0 +1,36 @@
+import {createElement, Text, Wrapper} from "./createElement";
+
+export class ListView {
+    constructor(config){
+        this.children = [];
+        this.attributes = new Map();
+        this.properties = new Map();
+        this.state = Object.create(null);
+    }
+
+    setAttribute(name, value) { //attribute
+        this[name] = value;
+    }
+
+    getAttribute(name) { //attribute
+        return this[name];
+    }
+
+    appendChild(child){
+        this.children.push(child);
+    }
+
+    render(){
+        let data = this.getAttribute('data');
+        let root = <div class="list-view" style="width:300px">
+            {
+                data.map(this.children[0])
+            }
+        </div>
+        return root;
+    }
+
+    mountTo(parent){
+        this.render().mountTo(parent);
+    }
+}
